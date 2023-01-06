50d0e34eed204a75cd8f60d325af829ad3e1a9da
Cristian-tec <crgs2008@gmail.com>
2023-01-06 01:05:10 -0300

    31

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,50 +1,42 @@
 import NavBar from "../NavBar/NavBar";
 import Footer from "../Footer/Footer";
+import { Box, Text, Button, IconButton } from "@chakra-ui/react";
+
 import { Link } from "react-router-dom";
 import "./Home.css";
 import Carousel from "../carousel/carousel";
-import { MdOutlineDeliveryDining } from "react-icons/md";
-import { Button, Box, Text, Image, Textarea } from "@chakra-ui/react";
+import { MdOutlineDeliveryDining } from "react-icons/md"
+
 
 export default function Home() {
+
+
   return (
     <div className="main-container-home">
       <div>
         <NavBar />
       </div>
-      <Box h={"500px"}>
-        <Box textAlign={"left"} mt={"150px"} ml={"100px"}>
-          <Text as={"b"} className="w-d-y">
-            WHAT DO YOU WANT TO ORDER<br></br> TODAY?
-          </Text>
+
+      <Box h={'500px'}>
+        <Box textAlign={'left'} className="text-box-w">
+          <Text as={'b'} className="w-d-y">WHAT DO YOU WANT TO ORDER<br></br> TODAY?</Text>
         </Box>
-        <Box ml={"100px"} mt={"50px"}>
+        <Box className="select-menu-chakra" >
           <Link to="/Selectmenu">
-            <Button
-              colorScheme={"red"}
-              size={"md"}
-              padding={"30px"}
-              borderRadius={"10px"}
-              fontSize={"30px"}
-            >
-              ORDER NOW&nbsp;&nbsp;&nbsp;
-              <MdOutlineDeliveryDining size={"40"} />
-            </Button>
+            <Button colorScheme={'red'} size={'md'} padding={'30px'} borderRadius={'10px'} fontSize={'30px'}>ORDER NOW&nbsp;&nbsp;&nbsp;<MdOutlineDeliveryDining size={'40'} /></Button>
           </Link>
         </Box>
       </Box>
 
-      <div className="container-directo">
+      <div className="container-directo112">
         <div className="acceso-directo11">
-          <Image
+          <img
             src="https://res.cloudinary.com/dqdrtbtuv/image/upload/v1671118229/mcburger/Prod11_outidc.png"
             alt="Imagen"
           />
           <h4>Classic Burger</h4>
           <Link to="/product/11">
-            <Button mt={"50px"} size={"lg"} fontSize={"20px"}>
-              Order Now
-            </Button>
+            <Button mt={'50px'} size={'lg'} fontSize={'20px'}>Order Now</Button>
           </Link>
         </div>
 
@@ -55,9 +47,7 @@ export default function Home() {
           />
           <h4>French Fries</h4>
           <Link to="/product/52">
-            <Button mt={"50px"} size={"lg"} fontSize={"20px"}>
-              Order Now
-            </Button>
+            <Button mt={'50px'} size={'lg'} fontSize={'20px'}>Order Now</Button>
           </Link>
         </div>
 
@@ -68,9 +58,7 @@ export default function Home() {
           />
           <h4>Offers</h4>
           <Link to="/product/64">
-            <Button mt={"50px"} size={"lg"} fontSize={"20px"}>
-              Order Now
-            </Button>
+            <Button mt={'50px'} size={'lg'} fontSize={'20px'}>Order Now</Button>
           </Link>
         </div>
       </div>
