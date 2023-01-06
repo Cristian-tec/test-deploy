901844957117c5d9ac4fb0669dc249ea0244230b
Cristian-tec <crgs2008@gmail.com>
2023-01-06 00:38:43 -0300

    30

--- api/src/app.js
+++ api/src/app.js
@@ -10,7 +10,7 @@ const localities = require("./routes/localities");
 const mercadoPago = require("./routes/mercadoPago");
 const orders = require("./routes/orders");
 const user = require("./routes/user")
-const cors = require('cors')
+const cors = require("cors");
 
 require("./db.js");
 
@@ -24,7 +24,7 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://test-deploy-topaz-nine.vercel.app"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
