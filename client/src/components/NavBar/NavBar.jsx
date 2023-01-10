399f1967bf4e6806186ad1656e4a7a453f009b47
Cristian-tec <crgs2008@gmail.com>
2023-01-10 15:22:07 -0300

    71

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./NavBar.css";
 import BurgerLogo from "./BurgerLogo.png";
 import { Link } from "react-router-dom";
 import { BsCart2 } from "react-icons/bs";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FaTimes } from "react-icons/fa";
 import CartList from "../CartList/CartList";
 import {
   Button,
@@ -20,141 +22,110 @@ import {
   PopoverAnchor,
 } from "@chakra-ui/react";
 
-import { useAuth0 } from '@auth0/auth0-react';
+export default function NavBar() {
+  const navRef = useRef();
+  const [scrollEnabled, setScrollEnabled] = useState(true);
 
-import { saveUser, eraseUser, userActive } from '../../actions/index'
-
-export default function NavBar(props) {
-
-  const dispatch = useDispatch();
-
-  const { loginWithRedirect, user, isAuthenticated, logout, isLoading } = useAuth0();
-  const userN = useSelector((state) => state.user);
-  const handleLogout = () => {
-    window.localStorage.setItem('userL', '');
-    logout();
+  function disableScroll() {
+    document.body.style.overflow = 'hidden';
   }
 
-  const getUserData = () => {
+  function enableScroll() {
+    document.body.style.overflow = 'auto';
+  }
 
-    let item = window.localStorage.getItem('userL');
-    let a;
-    if (item) {
-      a = JSON.parse(item);
+  const handleClick = () => {
+    showNavBar();
+    if (scrollEnabled) {
+      disableScroll();
+      setScrollEnabled(false);
     } else {
-      a = { email: '' };
+      enableScroll();
+      setScrollEnabled(true);
     }
+  }
 
-    if (a.email) {
-      //console.log('hay userN.email');
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener("resize", () => setWindowWidth(window.innerWidth));
+  }, []);
+
+  const showNavBar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
+
+  const userN = useSelector((state) => state.user);
+  //console.log(userN, '*');
+
+  const getUserData = () => {
+    if (userN) {
+      //console.log('entrando...');
       return (
         <>
-          <button onClick={() => handleLogout()} className="button-login"><b>LOG OUT</b></button>&nbsp;&nbsp;&nbsp;
           <label className="name-login">
-            <b>{a.name}</b>
+            <b>{userN.name}</b>
           </label>
-          &nbsp;&nbsp;&nbsp;
-          <img className="image-logo" src={a.picture} alt="image" />
+          &nbsp;&nbsp;
+          {/*  <img className="image-logo" src={userN.picture} alt="image" /> */}
         </>
       );
     } else {
-      //console.log('NO hay userN.email');
-      return (
-        <>
-          <Link to="/login">
-            <button colorScheme={'red'} className="button-login"><b>LOG IN</b></button>
-          </Link>
-        </>
-      );
+      return <></>;
     }
   };
 
-  const prueba = async () => {
-
-    let item = window.localStorage.getItem('userL');
-    if (item) {
-      console.log(JSON.parse(item));
-      let a = JSON.parse(item);
-      console.log(a.email, '<-');
-    }
-  }
-
-  useEffect(() => {
-
-  }, []);
-
-  const getFavorites = () => {
-    let item = window.localStorage.getItem('userL');
-    let a;
-    if (item) {
-      a = JSON.parse(item);
-    } else {
-      a = { email: '' };
-    }
-    if (a.email) {
-      return (
-        <>
-          <li><Link to="/favs">FAVORITES</Link></li>
-        </>
-      )
-    } else {
-      return (
-        <>
-          <li>FAVORITES</li>
-        </>
-      )
-    }
-  }
-
-  const getAdmin = () => {
-    let item = window.localStorage.getItem('userL');
-    let a;
-    if (item) {
-      a = JSON.parse(item);
-    } else {
-      a = { email: '' };
-    }
-    if (a.email === 'admin@mcburger.com') {
-      return (
-        <>
-          <a href="https://dashboard-wine-nine.vercel.app/" target="_blank" rel="noreferrer">
-            AD
-          </a>
-        </>
-      )
-    } else {
-      return (
-        <>
-        </>
-      )
-    }
-  }
+  useEffect(() => {}, [userN]);
 
   return (
-    <div>
-      <div className="nav-container">
-        <Link to="/">
-          <img className="burger-logo" src={BurgerLogo} alt="." />
-        </Link>
-        <ul>
-          <li>
-            <Link to="/">HOME</Link>
-          </li>
-          <li>
-            <Link to="/Selectmenu">MENU</Link>
-          </li>
-          <li>
-            <Link to="/news">NEWS</Link>
-          </li>
-          <li>
-            <Link to="/about">ABOUT US</Link>
-          </li>
-          {getFavorites()}
-          <li>
-            <Link to="/locations">LOCATIONS</Link>
-          </li>
-        </ul>
-        <Box>
+    <Box className="main-nav-container">
+      <Box className="nav-container">
+        <Box className="burger-menu">
+          <button className="nav-btn nav-btn-open" onClick={handleClick }>
+            <GiHamburgerMenu />
+          </button>
+        </Box>
+        <Box className="list-container">
+          <Link className="logo-nav" to="/">
+            <img src={BurgerLogo} alt="." />
+          </Link>
+        </Box>
+        <Box className="list-container">
+          <nav className="list-in-container" ref={navRef}>
+            <ul className="menu-list">
+              <li>
+                <Link to="/">HOME</Link>
+              </li>
+              <li>
+                <Link to="/Selectmenu">MENU</Link>
+              </li>
+              <li>
+                <Link to="/news">NEWS</Link>
+              </li>
+              <li>
+                <Link to="/about">ABOUT US</Link>
+              </li>
+              <li>
+                <Link to="/favs">FAVORITES</Link>
+              </li>
+              <li>
+                <Link to="/locations">LOCATIONS</Link>
+              </li>
+            </ul>
+            {windowWidth <= 930 && (
+              <Box>
+                <Box className="log-in">
+                  <Link to="/login">
+                    <Button colorScheme={"red"} className="button-login">
+                      <Text textDecoration={"none"}>LOG IN</Text>
+                    </Button>
+                  </Link>
+                </Box>
+              </Box>
+            )}
+          </nav>
+        </Box>
+        <Box className="cart-button">
           <Popover isLazy trigger="hover">
             <PopoverTrigger>
               <Button>
@@ -173,14 +144,18 @@ export default function NavBar(props) {
             </PopoverContent>
           </Popover>
         </Box>
-        &nbsp;&nbsp;&nbsp;
-
         {getUserData()}
-        &nbsp;&nbsp;
-        {getAdmin()}
 
-        {/*  <Button onClick={() => prueba()} colorScheme={'red'} className="button-login"><Text textDecoration={'none'}>PRUEBA</Text></Button> */}
-      </div>
-    </div>
+        {windowWidth > 930 && (
+          <Box className="log-in">
+            <Link to="/login">
+              <Button colorScheme={"red"} className="button-login">
+                <Text textDecoration={"none"}>LOG IN</Text>
+              </Button>
+            </Link>
+          </Box>
+        )}
+      </Box>
+    </Box>
   );
 }
